037b4b400f504d4eee3741f84445ca29b39a5a3d
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-07-03 19:15:25 -0300

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,16 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    ignores: ["eslint.config.mjs"],
+  },
+  {
+    settings: {
+      react: {
+        version: "19.2.7"
+      }
+    }
+  }
 ]);
 
 export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Montserrat, Geist } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import "./globals.css";
-
-const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 const montserrat = Montserrat({
   subsets: ['latin']
