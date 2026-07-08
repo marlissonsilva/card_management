1a51b45723c5783d3757cc1e337e6dd282b54546
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-07-08 13:57:02 -0300

    feat: add step run tests in ci

--- src/backend/User/create.ts
+++ src/backend/User/create.ts
@@ -6,5 +6,9 @@ interface CreateUserProps {
 }
 export default function CreateUser({ username, email, password, invoice_closing }: CreateUserProps): { message: string } {
   console.log(username, email, password, invoice_closing)
+
+  if (!username || !email || !password || !invoice_closing) {
+    return { message: "Erro ao criar usuário" }
+  }
   return { message: "Usuário criado com sucesso" }
 }
