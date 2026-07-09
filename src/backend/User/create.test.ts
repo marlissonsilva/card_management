85b936577d4860ebb42f8a7ab85472e299d16309
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-07-09 13:53:09 -0300

    feat: implement saved in database and add mock in test

--- src/backend/User/create.test.ts
+++ src/backend/User/create.test.ts
@@ -1,22 +1,45 @@
+/**
+ * @jest-environment node
+ */
 import '@testing-library/jest-dom'
-import CreateUser from "./create";
+import { CreateUser } from './create'
+import prisma from '@/src/lib/prisma'
+import bcrypt from 'bcryptjs'
 
+jest.mock('../../lib/prisma.ts', () => ({
+  __esModule: true,
+  default: {
+    user: {
+      create: jest.fn()
+    }
+  }
+}))
 
 describe('CreateUser', () => {
-  it('Deve retornar a mensagem de sucesso ao criar de usuário', () => {
+  it('Deve retornar a mensagem de sucesso ao criar de usuário', async () => {
     const data = {
       username: 'fulano',
       email: 'test@gmail.com',
       password: 'passworD37#',
       invoice_closing: 15,
-    }
+    };
 
-    const result = CreateUser(data)
+    (prisma.user.create as jest.Mock).mockResolvedValue({
+      uuid: '1234-uuid',
+      username: data.username,
+      password: await bcrypt.hash(data.password, 10),
+      email: data.email,
+      invoice_closing: data.invoice_closing
+    })
+
+    const result = await CreateUser(data)
 
     expect(result).toEqual({ message: "Usuário criado com sucesso" })
+
+    expect(prisma.user.create).toHaveBeenCalled()
   })
 
-  it("Deve retornar o erro ao criar usuário", () => {
+  it("Deve retornar o erro ao criar usuário", async () => {
     const data = {
       username: '',
       email: '',
@@ -24,7 +47,7 @@ describe('CreateUser', () => {
       invoice_closing: 20
     }
 
-    const result = CreateUser(data)
+    const result = await CreateUser(data)
 
     expect(result).toEqual({ message: 'Erro ao criar usuário' })
   })
--- src/backend/User/create.ts
+++ src/backend/User/create.ts
@@ -1,24 +1,41 @@
+"use server"
+
 import bcrypt from "bcryptjs";
 import { passwordValidate } from "../utils/password-validate";
+import prisma from "@/src/lib/prisma";
+
 interface CreateUserProps {
   username: string
   email: string
   password: string
   invoice_closing: number
 }
-export default function CreateUser({ username, email, password, invoice_closing }: CreateUserProps): { message: string } {
-  const isMatchPassword = passwordValidate(password)
 
-  if (!isMatchPassword) {
-    return { message: "A senha não atende ao critérios." }
-  }
-
-  const encrypt_password = bcrypt.hashSync(password)
-  console.log(encrypt_password)
-
+export async function CreateUser({ username, email, password, invoice_closing }: CreateUserProps): Promise<{ message: string }> {
   if (!username || !email || !password || !invoice_closing) {
     return { message: "Erro ao criar usuário" }
   }
 
-  return { message: "Usuário criado com sucesso" }
+  const isMatchPassword = passwordValidate(password)
+  if (!isMatchPassword) {
+    return { message: "A senha não atende ao critérios." }
+  }
+
+  try {
+    const encrypt_password = await bcrypt.hash(password, 10)
+
+    const data = {
+      username,
+      email,
+      password: encrypt_password,
+      invoice_closing
+    }
+
+    const result = await prisma.user.create({ data })
+    console.log("result", result)
+    return { message: "Usuário criado com sucesso" }
+  } catch (error) {
+    console.log(error)
+    return { message: "Erro ao criar usuário" }
+  }
 }
