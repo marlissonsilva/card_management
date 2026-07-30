d0c2918c3ec7065ba08529547b1fb7e2d9da63d7
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-07-30 15:24:33 -0300

    feat: function logout

---
+++ src/backend/User/logout.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function logout() {
+  ((await cookies()).delete("session"));
+  redirect("/login")
+}
