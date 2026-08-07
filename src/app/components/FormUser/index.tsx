97040f9f44b674ceb1508166b5e4c2d1e979e004
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-08-07 16:57:48 -0300

    feat: update formUser styles

--- src/app/components/FormUser/index.tsx
+++ src/app/components/FormUser/index.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import { authenticate } from '@/src/backend/User/authenticate'
 import { createUser } from '@/src/backend/User/create'
 import { useRouter } from 'next/navigation'
+import styles from './FormUser.module.css'
 
 export function FormUser() {
   const [action, setAction] = useState<"login" | "account">("login")
@@ -36,54 +37,71 @@ export function FormUser() {
   }
 
   return (
-    <section>
-      <div className='flex w-full border rounded-sm overflow-hidden'>
+    <section className={styles.section}>
+      <div className={styles.tabs_container}>
         <button
-          className={`p-2 flex-1 -mr-6 [clip-path:polygon(0%_0%,100%_0%,90%_100%,0%_100%)] 
-                      ${!isLogin ? "bg-gray-500 text-white font-semibold" : ""}`}
-          onClick={() => {
-            setAction("account")
-            reset()
-          }}
+          className={`${styles.tab_button} ${styles.tab_left} ${!isLogin ? styles.tab_active : ''}`}
+          onClick={() => { setAction("account"); reset(); }}
         >
-          <span className="inline-block italic">Criar conta</span>
+          <span className={styles.tab_text}>Criar conta</span>
         </button>
+
+        {/* Botão Fazer Login */}
         <button
-          className={`p-2 flex-1 [clip-path:polygon(10%_0%,100%_0%,100%_100%,0%_100%)]  
-                      ${isLogin ? "bg-gray-500 text-white font-semibold" : ""}`}
-          onClick={() => {
-            setAction("login")
-            reset()
-          }}>
-          <span className="inline-block italic">Fazer login</span>
+          className={`${styles.tab_button} ${styles.tab_right} ${isLogin ? styles.tab_active : ''}`}
+          onClick={() => { setAction("login"); reset(); }}
+        >
+          <span className={styles.tab_text}>Fazer login</span>
         </button>
+
       </div>
-      <form className="w-md flex flex-col justify-center py-4 gap-3" onSubmit={handleSubmit(onSubmit)}>
-        {!isLogin && <div className="flex flex-col">
-          <label>Digite seu nome
-            <input type="text" {...register("username")} />
-          </label>
-          {errors.username?.message && <span className='text-red-600 text-sm' >{errors.username?.message}</span>}
-        </div>}
-        <div className="flex flex-col">
-          <label>Digite seu email
-            <input type="email" {...register('email')} />
-          </label>
-          {errors.email?.message && <span className='text-red-600 text-sm'>{errors.email?.message}</span>}
+
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+
+        {!isLogin && (
+          <div className={styles.input_group}>
+            <label>Digite seu nome <input type="text" {...register("username")} /></label>
+            {errors.username?.message && (
+              <span className={styles.error_message}>{errors.username.message}</span>
+            )}
+          </div>
+        )}
+
+        <div className={styles.input_group}>
+          <label>Digite seu email <input type="email" {...register('email')} /></label>
+          {errors.email?.message && (
+            <span className={styles.error_message}>{errors.email.message}</span>
+          )}
         </div>
-        <div className="flex flex-col">
-          <label>Digite sua senha
-            <input type="text" {...register("password")} />
-          </label>
-          {errors.password?.message && <span className='text-red-600 text-sm'>{errors.password?.message}</span>}
+
+        <div className={styles.input_group}>
+          <label>Digite sua senha <input type="password" {...register("password")} /></label>
+          {errors.password?.message && (
+            <span className={styles.error_message}>{errors.password.message}</span>
+          )}
         </div>
-        {!isLogin && <div className="flex flex-col">
-          <label>Data de fechamento da fatura
-            <input type="number" min="1" max="31" onChange={(event) => setValue("invoice_closing", Number(event.target.value))} />
-          </label>
-          {errors.invoice_closing?.message && <span className='text-red-600 text-sm'>{errors.invoice_closing?.message}</span>}
-        </div>}
-        <button type='submit' className='border p-4 rounded-sm mt-2'>{action === "account" ? "Criar conta" : "Fazer login"}</button>
+
+        {!isLogin && (
+          <div className={styles.input_group}>
+            <label>
+              Data de fechamento da fatura
+              <input
+                type="number"
+                min="1"
+                max="31"
+                onChange={(event) => setValue("invoice_closing", Number(event.target.value))}
+              />
+            </label>
+            {errors.invoice_closing?.message && (
+              <span className={styles.error_message}>{errors.invoice_closing.message}</span>
+            )}
+          </div>
+        )}
+
+        <button type='submit' className={styles.submit_button}>
+          {action === "account" ? "Criar conta" : "Fazer login"}
+        </button>
+
       </form>
     </section>
 
