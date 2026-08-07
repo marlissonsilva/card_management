24941ce0e8f14a3ce18b08226410c5bd0accc165
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-08-07 16:56:30 -0300

    feat: add findUnique query for existing users

--- src/backend/User/create.ts
+++ src/backend/User/create.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import bcrypt from "bcryptjs";
 import { passwordValidate } from "../utils/password-validate";
@@ -6,31 +6,43 @@ import prisma from "@/src/lib/prisma";
 import { userSchema } from "./validate/zod";
 import { CreateUserProps } from "./types";
 
-export async function createUser(formData: CreateUserProps): Promise<{ success: boolean, message: string }> {
-  const validateUser = userSchema.safeParse(formData)
+export async function createUser(
+  formData: CreateUserProps,
+): Promise<{ success: boolean; message: string }> {
+  const validateUser = userSchema.safeParse(formData);
   if (!validateUser.success) {
-    return { success: false, message: "Erro ao criar usuário" }
+    return { success: false, message: "Erro ao criar usuário" };
   }
 
-  const { username, email, password, invoice_closing } = validateUser.data
-  const isMatchPassword = passwordValidate(password)
+  const { username, email, password, invoice_closing } = validateUser.data;
+
+  const user = await prisma.user.findUnique({
+    where: { email: formData.email },
+  });
+
+  if (user) {
+    return { success: false, message: "Usuário já possui conta cadastrada!" };
+  }
+
+  const isMatchPassword = passwordValidate(password);
   if (!isMatchPassword) {
-    return { success: false, message: "A senha não atende ao critérios." }
+    return { success: false, message: "A senha não atende ao critérios." };
   }
 
   try {
-    const encrypt_password = await bcrypt.hash(password, 10)
+    const encrypt_password = await bcrypt.hash(password, 10);
 
     const data = {
       username,
       email,
       password: encrypt_password,
-      invoice_closing
-    }
+      invoice_closing,
+    };
 
-    await prisma.user.create({ data })
-    return { success: true, message: "Usuário criado com sucesso" }
+    await prisma.user.create({ data });
+    return { success: true, message: "Usuário criado com sucesso" };
   } catch (error) {
-    return { success: false, message: "Erro ao criar usuário" }
+    console.log(error);
+    return { success: false, message: `Erro ao criar usuário, ${error}` };
   }
 }
