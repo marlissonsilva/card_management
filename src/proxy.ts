7473916fa62e12dcc78c369c8166d94f72580890
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-08-07 16:59:16 -0300

    feat: add proxy.ts to handle session and cookies

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,20 +1,19 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const token = request.cookies.get('session')?.value;
-  const { pathname } = request.nextUrl
-  if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  const token = request.cookies.get("session")?.value;
+  const { pathname } = request.nextUrl;
+  if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  if (token && (pathname === '/login' || pathname === '/')) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+  if (token && (pathname === "/login" || pathname === "/")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/']
+  matcher: ["/dashboard/:path*", "/login", "/"],
 };
