d0221146524b20f8a034f255856da4d085bf00d7
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-08-25 12:39:47 -0300

    feat: rename function findAll member to findMemberByMonth

---
+++ src/backend/Member/findMembersByMonth.ts
@@ -0,0 +1,81 @@
+"use server";
+
+import prisma from "@/src/lib/prisma";
+import { getSession } from "../User/session";
+import { getMonthDate } from "../utils/date";
+import { getUser } from "../User/getUser";
+
+export async function findMembersByMonth(month: number) {
+  const userUuid = await getSession();
+
+  if (!userUuid?.uuid) return [];
+
+  const user = await getUser(userUuid.uuid);
+
+  if (!user) {
+    throw new Error("Erro ao buscar usuário");
+  }
+
+  const dates = getMonthDate(user.invoice_closing, month);
+
+  try {
+    const response = await prisma.member.findMany({
+      where: {
+        user_uuid: userUuid?.uuid,
+        purchases: {
+          some: {
+            installment: {
+              some: {
+                due_date: {
+                  gte: dates.startDate,
+                  lte: dates.endDate,
+                },
+              },
+            },
+          },
+        },
+      },
+      select: {
+        uuid: true,
+        name: true,
+        purchases: {
+          select: {
+            installment: {
+              where: {
+                due_date: {
+                  gte: dates.startDate,
+                  lte: dates.endDate,
+                },
+              },
+              select: {
+                value: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        name: "asc",
+      },
+    });
+
+    return response.map((member) => {
+      const totalAmountPurchases = member.purchases.reduce((acc, purchase) => {
+        const sumOfInstallments = purchase.installment.reduce(
+          (instAcc, installment) => instAcc + installment.value,
+          0,
+        );
+        return acc + sumOfInstallments;
+      }, 0);
+
+      const { ...memberData } = member;
+      return {
+        ...memberData,
+        totalAmountPurchases,
+      };
+    });
+  } catch (error) {
+    console.error(`Erro ao buscar membros, ${error}`);
+    return [];
+  }
+}
