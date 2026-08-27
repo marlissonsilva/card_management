90a2482a520772872602af445c76a995018d9eb3
marlyssonsilva <marlyssonsilva005@gmail.com>
2026-08-27 12:13:31 -0300

    feat: update pages

--- src/app/(pages)/dashboard/compras/page.tsx
+++ src/app/(pages)/dashboard/compras/page.tsx
@@ -1,44 +1,18 @@
 "use client";
 import { CardPurchase } from "@/src/app/components/CardPurchase";
-import { useCreatePurchaseStore } from "@/src/app/store/useCreatePurchase";
-import { useModalStore } from "@/src/app/store/useModal";
-import { useMonthStore } from "@/src/app/store/useMonth";
-import { currencyFormat } from "@/src/app/utils/currencyFormat";
-import { findInstallmentsByMonth } from "@/src/backend/Installment/findInstallmentsByMonth";
-import { useEffect, useState } from "react";
-
-type InstallmentsItem = Awaited<
-  ReturnType<typeof findInstallmentsByMonth>
->["data"][number];
+import { useInstallments } from "@/src/app/hooks/useInstallments";
 
 export default function Page() {
-  const created = useCreatePurchaseStore((state) => state.created);
-  const isOpen = useModalStore((state) => state.isOpen);
-  const [installments, setInstallments] = useState<InstallmentsItem[]>([]);
-  const [totalAmount, setTotalAmount] = useState<number>(0);
-  const month = useMonthStore((state) => state.month);
-  console.log({ month });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await findInstallmentsByMonth(month);
-      setTotalAmount(response.totalAmount);
-      setInstallments(response.data);
-    };
-    fetchData();
-  }, [created, isOpen, month]);
+  const installments = useInstallments();
 
   return (
     <section className="flex flex-col gap-2">
-      <div className="flex justify-end">
-        <p>
-          Fatura atual:
-          <span className="font-semibold text-xl p-1"> {currencyFormat(totalAmount)}</span>
-        </p>
-      </div>
-      {installments.map((item) => (
+      {installments.data.map((item) => (
         <CardPurchase key={item.uuid} data={item} />
       ))}
+      {installments.data.length === 0 && (
+        <div className="text-center pt-20">Sem compras cadastradas!</div>
+      )}
     </section>
   );
 }
--- src/app/(pages)/dashboard/membros/page.tsx
+++ src/app/(pages)/dashboard/membros/page.tsx
@@ -1,29 +1,18 @@
 "use client";
 import { CardMember } from "@/src/app/components/CardMember";
-import { useCreatePurchaseStore } from "@/src/app/store/useCreatePurchase";
-import { useMonthStore } from "@/src/app/store/useMonth";
-import { findMembersByMonth } from "@/src/backend/Member/findMembersByMonth";
-import { useEffect, useState } from "react";
-
-type Member = Awaited<ReturnType<typeof findMembersByMonth>>[number];
+import { useMembers } from "@/src/app/hooks/useMembers";
 
 export default function Page() {
-  const created = useCreatePurchaseStore((state) => state.created);
-  const [members, setMembers] = useState<Member[]>([]);
-  const month = useMonthStore((state) => state.month);
-  useEffect(() => {
-    const fetchData = async () => {
-      const members = await findMembersByMonth(month);
-      setMembers(members);
-    };
-    fetchData();
-  }, [created, month]);
+  const members = useMembers();
 
   return (
     <section className="flex flex-col gap-2">
-      {members.map((member) => (
+      {members.data.map((member) => (
         <CardMember key={member.uuid} data={member} />
       ))}
+      {members.data.length === 0 && (
+        <div className="text-center pt-20">Sem membros cadastrados!</div>
+      )}
     </section>
   );
 }
